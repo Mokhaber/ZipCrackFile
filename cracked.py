2c8752bce87b9387ea5f87c8d1563d04b48b47dd
Mohammadali <157125680+Mokhaber@users.noreply.github.com>
2024-04-20 16:13:38 +0330

    Add files via upload
    Code

---
+++ cracked.py
@@ -0,0 +1,16 @@
+import zipfile
+
+zip_file = "A.zip"  # Replace "example.zip" with the name of your zip file
+password_file = "C://Users/dena8/Desktop/New folder/important-list.txt"
+
+with open(password_file, 'r', encoding='utf-8') as file:
+    password_list = [line.strip() for line in file]
+
+for password in password_list:
+    try:
+        with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+            zip_ref.extractall(pwd=bytes(password, 'utf-8'))
+        print("Success! Password is:", password)
+        break
+    except Exception as e:
+        print("Tried", password, "but failed.")
